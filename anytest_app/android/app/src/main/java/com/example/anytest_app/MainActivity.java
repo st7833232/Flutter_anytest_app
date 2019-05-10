65ccc9ac8e56d020fd22396efc609f7f087f1fea
1500428 <jeffcheng@systex.com>
2019-05-10 18:23:08 +0800

    [20190510]補commit main/java/com/example/anytest_app/MainActivity.java

--- anytest_app/android/app/src/main/java/com/example/anytest_app/MainActivity.java
+++ anytest_app/android/app/src/main/java/com/example/anytest_app/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.anytest_app;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "com.example.anytest_app/anytest_app";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler((methodCall, result) -> {
+      if (methodCall.method.equals("TestCode")) {
+        String val = methodCall.argument("name");
+        if (val.equals("Java Code Test")) {
+          result.success("Return from Java");
+        }
+        else {
+          result.error("Error", "Error", "Error");
+        }
+      }
+    });
   }
 }
